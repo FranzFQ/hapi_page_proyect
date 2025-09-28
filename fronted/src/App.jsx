b3b451f53ad57b5d2a85c6d99a0320a7c640e623
JavierSanchez22 <fjsanchez@correo.url.edu.gt>
2025-09-27 23:36:56 -0600

    feat: add BrowserRouter with routes for UserHome and UserRegister

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,9 +1,18 @@
-import UserRegister from "./user-components/UserRegister"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserRegister from "./user-components/UserRegister.jsx";
+import UserHome from "./user-components/UserHome.jsx";
 
 export default function App() {
   return (
-    <div className="app-container">
-      <UserRegister />
-    </div>
-  )
+    <Router>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <Routes>
+          <Route path="/" element={<UserHome />} />
+          <Route path="/home" element={<UserHome />} />
+          <Route path="/register" element={<UserRegister />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }

05cab055c45b05be5008697991f9f6300bb11f87
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-10-03 18:51:01 -0600

    feat: Update App.jsx with routes for login and register pages

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import UserRegister from "./user-components/UserRegister"
+import { EmailLoginPage } from "./pages/EmailLoginPage"
+import { PasswordLoginPage } from "./pages/PasswordLoginPage"
 
 export default function App() {
   return (
-    <div className="app-container">
-      <UserRegister />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/loginEmail" />} />
+        <Route path="loginEmail" element={<EmailLoginPage />} />
+        <Route path="loginPassword" element={<PasswordLoginPage />} />
+        <Route path="register" element={<UserRegister />} />
+      </Routes>
+    </BrowserRouter>
   )
 }

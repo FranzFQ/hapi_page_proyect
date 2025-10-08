dc01934bc514821437a262b52b8c71e11d2f66ec
JavierSanchez22 <fjsanchez@correo.url.edu.gt>
2025-10-08 15:47:54 -0600

    feat(admin): implement admin dashboard view
    Creates the new admin section, including a sidebar, stat cards, an actions list, and a dynamic analytics chart. Adds the /admin route to the main application router.

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -4,19 +4,19 @@ import UserRegister from "./user-components/UserRegister.jsx";
 import UserHome from "./pages/UserHome.jsx";
 import EmailLoginPage from "./pages/EmailLoginPage.jsx";
 import PasswordLoginPage from "./pages//PasswordLoginPage.jsx"
+import AdminDashboard from "./pages/AdminDashboard.jsx";
 
 export default function App() {
   return (
     <Router>
-      <div style={{ width: '100vw', height: '100vh' }}>
         <Routes>
           <Route path="/" element={<UserHome />} />
           <Route path="/home" element={<UserHome />} />
           <Route path="/loginEmail" element={<EmailLoginPage />} />
           <Route path="/loginPassword" element={<PasswordLoginPage />} />
           <Route path="/register" element={<UserRegister />} />
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
-      </div>
     </Router>
   );
 }

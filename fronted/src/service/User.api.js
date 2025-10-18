8a58e8b2804439c7a113c798ce31a62d60739ef1
Franz <124092913+FranzFQ@users.noreply.github.com>
2025-10-18 13:57:59 -0600

    fix: correction style to creat a request in frontend

--- fronted/src/service/User.api.js
+++ fronted/src/service/User.api.js
@@ -1,20 +1,11 @@
 import axios from "axios";
 
-export const registerUser = async (userData) => {
-    return await axios.post(
-      "http://127.0.0.1:8000/users/users/",
-      userData
-    );
-};
+const userApi = axios.create({
+  baseURL: "http://127.0.0.1:8000/users",
+});
 
-export const clientLogin = async (loginData) => {
-  try {
-    const response = await axios.post(
-      "http://127.0.0.1:8000/users/client-profiles/",
-      loginData
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
+export const registerUser = async (userData) =>
+  userApi.post("users/", userData);
+
+export const clientLogin = async (loginData) =>
+  userApi.post("client-profiles/", loginData);

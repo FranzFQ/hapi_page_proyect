041d297415cd014f30c46161fb1866f0efb99da3
Franz <124092913+FranzFQ@users.noreply.github.com>
2025-10-20 23:09:52 -0600

    feat: add models in admin.py

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -3,13 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserHome from "./pages/UserHome.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import UserRegister from "./pages/UserRegister.jsx";
-import AdminDashboard from "./pages/AdminDashboard.jsx";
-import EmailLoginPage from "./pages/EmailLoginPage.jsx";
-import PasswordLoginPage from "./pages/PasswordLoginPage.jsx";
-import ManageStocksPage from './pages/ManageStocksPage';
-import AddStockPage from './pages/AddStockPage';
-import EditStockPage from './pages/EditStockPage';
-import StockDetailsPage from './pages/StockDetailsPage';
 
 export default function App() {
   return (

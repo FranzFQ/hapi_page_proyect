7a8547aff1c5a04ae1b3da28ebc8672cc9a03ddd
Karla Lopez <kalselizabethlopez@gmail.com>
2025-10-23 23:34:35 -0600

    feat: add LandingPage view and base styles
    Introduces LandingPage and stylesheet with initial layout following mockups.

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import UserHome from "./pages/UserHome.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
 import UserRegister from "./user-components/UserRegister.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
 import EmailLoginPage from "./pages/EmailLoginPage.jsx";
@@ -15,13 +16,14 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserHome />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="/home" element={<UserHome />} />
         <Route path="/register" element={<UserRegister />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/loginEmail" element={<EmailLoginPage />} />
         <Route path="/loginPassword" element={<PasswordLoginPage />} />
-
+        
         <Route path="/admin/stocks" element={<ManageStocksPage />} />
         <Route path="/admin/stocks/add" element={<AddStockPage />} />
         <Route path="/admin/stocks/edit/:stockId" element={<EditStockPage />} />
